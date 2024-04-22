5f280306c72a5a8fad510c73aceacc4a15c607ff
Aldin Huric <alhuric89@gmail.com>
2024-04-22 14:16:58 +0200

    initial commit

---
+++ src/utils/firebase.utils.jsx
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+	apiKey: 'AIzaSyD70X_QS0A74_5XlwMT5vrkGdhZljBnnNw',
+	authDomain: 'fir-9-4f93b.firebaseapp.com',
+	projectId: 'fir-9-4f93b',
+	storageBucket: 'fir-9-4f93b.appspot.com',
+	messagingSenderId: '512942131477',
+	appId: '1:512942131477:web:88a18a9c795de463aa411c',
+};
